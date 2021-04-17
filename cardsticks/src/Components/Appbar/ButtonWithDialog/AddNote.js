55fe732bc8a051fced7f3b6a46a8d1a811725caf
Tushar Tiwari <tushartiwari0172@gmail.com>
2021-04-17 17:16:23 +0530

    add: Account and Add Notes

---
+++ cardsticks/src/Components/Appbar/ButtonWithDialog/AddNote.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import BasicButton from '../BasicButton'
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import BasicButtonWithMargin from '../BasicButtonWithMargin'
+
+ const AddNote = (props) => {
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+     return (
+         <>
+        <BasicButton  color="inherit" onClick={handleClickOpen}>
+          {props.children}
+        </BasicButton>
+
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">{props.children}</DialogTitle>
+        <DialogContent>
+          
+          <TextField
+            autoFocus
+            margin="none"
+            id="Note"
+            label="Note"
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions justifyContent="start" spacing={2}>
+          <BasicButtonWithMargin variant="contained" onClick={handleClose} color="primary">
+            {props.children}
+          </BasicButtonWithMargin>
+        </DialogActions>
+      </Dialog>
+      </>
+     );
+ }
+ export default AddNote;
